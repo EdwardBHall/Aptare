9b33f0f01566d1fcc1f6b620ab11bc0c19129306
Edward Hall <hall.edward.b@gmail.com>
2022-06-29 21:06:05 -0500

    job type group by not working

---
+++ Aptare Reports/Victoria/Job_Type_Investigation_V1.sql
@@ -0,0 +1,15 @@
+--Author: ehall@silexdata.com
+--Last Modified: 06/01/2022
+SELECT
+--MAX(COUNT(*)) records,
+s.server_id,
+MAX(jd.job_type) crap,
+jd.job_type,                       -- Only included to speed query not needed to use host scope
+jd.job_type_name,                  -- Diff, Full etc
+jd.start_date,                     -- Start Date used only in scope selection not displayed
+jd.finish_date                     -- End Date used only in scope selection not displayed
+FROM apt_v_server s, apt_v_nbu_job_detail jd
+WHERE jd.finish_date BETWEEN ${startDate} AND ${endDate}
+AND s.server_id IN (${hosts})      -- Variable where master server can be set
+AND s.server_id = jd.server_id
+GROUP BY jd.job_type
