ee3cdab36cbfe49c0b0dbaaf5df2299bcdb9fc38
Edward Hall <hall.edward.b@gmail.com>
2022-06-29 21:54:36 -0500

    V2 and in progress.png

---
+++ Aptare Reports/Victoria/Job_Type_Investigation_V2.sql
@@ -0,0 +1,23 @@
+--Author: ehall@silexdata.com
+--Last Modified: 06/01/2022
+WITH t1 AS (
+SELECT
+--COUNT(*) records,
+jd.job_id,                         -- Included only as primary speed key
+jd.job_type AS type,                       -- Only included to speed query not needed to use host scope
+jd.job_type_name AS name,                  -- Diff, Full etc
+jd.start_date,                     -- Start Date used only in scope selection not displayed
+jd.finish_date                     -- End Date used only in scope selection not displayed
+FROM apt_v_server s, apt_v_nbu_job_detail jd
+WHERE jd.finish_date BETWEEN ${startDate} AND ${endDate}
+AND jd.server_id = s.server_id
+AND s.server_id IN (${hosts})      -- Variable where the master server can be set
+),
+t2 AS (
+SELECT
+type,
+name
+FROM t1
+GROUP BY type, name
+)
+SELECT * FROM t2
