671ab1a214ecc523ef105e78ac0670c75e5b5f93
Edward Hall <hall.edward.b@gmail.com>
2022-07-11 14:07:07 -0500

    V2 Prolicys_jobs

--- Aptare Reports/Victoria/Policys_Jobs_Fails.sql
+++ Aptare Reports/Victoria/Policys_Jobs_Fails.sql
@@ -1,11 +1,11 @@
 --Author: ehall@silexdata.com
---Last Modified: 06/07/2022
+--Last Modified: 07/11/2022
 WITH t1 AS (
 SELECT
-jd.job_id,                         -- Primary key in this view. Will this speed it up?
+jd.job_id,                         -- Primary key in this view. Used to count # of APTARE Jobs
 s.hostname master_server,          -- Master Server apt_v_server
 jd.policy_name,
-jd.client_id,                      
+jd.client_id,                     
 jd.summary_status                          
 FROM apt_v_server s, apt_v_nbu_job_detail jd, apt_v_nbu_schedule sc   -- Job Detail contains Policy Name
 WHERE jd.finish_date BETWEEN ${startDate} AND ${endDate}
@@ -15,11 +15,11 @@ AND jd.schedule_id = sc.schedule_id                    -- Key between tables
 AND jd.job_type NOT IN(103, 114)    -- Exclude Catalog Backup and Tape Drive Cleaning
 )
 SELECT
-t1.master_server,
-t1.policy_name,
-COUNT(t1.client_id),
-COUNT(t1.policy_name),
-SUM(CASE WHEN t1.summary_status = 2 THEN 1 ELSE 0 END ) Failed_Jobs   -- Show case statement usage
+t1.master_server,                   -- Name of Master Server
+t1.policy_name,                     -- Name of Policy
+COUNT(t1.client_id),                -- # of Clients in Policy
+COUNT(t1.job_id),                   -- # of Jobs in Policy
+SUM(CASE WHEN t1.summary_status = 2 THEN 1 ELSE 0 END ) Failed_Jobs   -- # of Failed Jobs
 FROM t1
 GROUP BY t1.master_server, t1.policy_name
-ORDER BY t1.master_server
+ORDER BY 1
